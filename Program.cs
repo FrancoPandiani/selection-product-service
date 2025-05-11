c2e13f173899cb8f3f86d430b551dd3ea6a788c6
Frank <franco.dan.dev@gmail.com>
2025-05-10 23:22:28 -0300

    commit 011 - configure API services and middleware: controllers, Swagger and localization

--- Program.cs
+++ Program.cs
@@ -1,24 +1,33 @@
 using System.Globalization;
 using Microsoft.AspNetCore.Localization;
+using Microsoft.EntityFrameworkCore;
+using Selection.ProductService.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Prepara la app para exponer y visualizar automáticamente la documentación de las APIs REST.
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 // Restringe las culturas admitidas tanto para los datos como para la UI solo a "en-US".
 builder.Services.Configure<RequestLocalizationOptions>(options =>
 {
-options.DefaultRequestCulture = new RequestCulture("en-US");
-options.SupportedCultures = new[] { new CultureInfo("en-US") };
-options.SupportedUICultures = new[] { new CultureInfo("en-US") };
+    options.DefaultRequestCulture = new RequestCulture("en-US");
+    options.SupportedCultures = new[] { new CultureInfo("en-US") };
+    options.SupportedUICultures = new[] { new CultureInfo("en-US") };
 });
+
+// Conexión a SQL server
+builder.Services.AddDbContext<ProductContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Habilita el uso de controladores que derivan de ControllerBase o Controller.
+builder.Services.AddControllers();
 
 var app = builder.Build();
 app.UseRequestLocalization();
 
-// Configure the HTTP request pipeline.
+// Activa la documentación interactiva de Swagger solo cuando la aplicación está en modo desarrollo.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -26,4 +35,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
 app.Run();
